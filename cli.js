e136c258f72e8013ed25ded001decbd96863d195
pomber <pombopombopombo@gmail.com>
2017-02-12 18:54:09 -0300

    Call multi-copy

---
+++ cli.js
@@ -0,0 +1,26 @@
+#!/usr/bin/env node
+'use strict';
+const fs = require('fs');
+const meow = require('meow');
+const multicopy = require('multi-copy');
+
+const usage = `
+	Usage
+	  $ mucopy <configfile>
+	Examples
+	  $ mucopy 'backup.json'
+`;
+
+const cli = meow(usage, {string: ['_']});
+const configpath = cli.input[0];
+
+fs.readFile(configpath, (err, data) => {
+	if (err) {
+		throw err;
+	}
+	const config = JSON.parse(data);
+	multicopy(config).catch(err => {
+		throw err;
+	});
+});
+
